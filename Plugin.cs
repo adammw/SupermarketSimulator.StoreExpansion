69e354d36a95a70bc2ab8adb64f1477006eff98e
Adam Malcontenti-Wilson <adman.com@gmail.com>
2025-03-10 15:07:47 +1100

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace StoreExpansion
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource Log;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+            Plugin.Log = Logger;
+        }
+    }
+}
