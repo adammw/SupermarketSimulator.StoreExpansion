15449892b24bdbfd7ea6533aeeeadfce63bf750b
Adam Malcontenti-Wilson <adman.com@gmail.com>
2025-04-10 19:29:51 +1000

    Initial code

---
+++ Patches.cs
@@ -0,0 +1,114 @@
+﻿using __Project__.Scripts.FloorPaintSystem;
+using __Project__.Scripts.Managers;
+using HarmonyLib;
+using MyBox;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Unity;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements.UIR;
+
+namespace StoreExpansion
+{
+    [HarmonyPatch]
+    class GrowthTabPatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GrowthTab), "Start")]
+        public static void StartPrefix()
+        {
+            Plugin.Log.LogInfo("GrowthTab Start");
+        }
+    }
+
+    [HarmonyPatch]
+    class PlayerInteractionPatches
+    {
+        static bool hiddenBuildings = false;
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerInteraction), "Update")]
+        public static void UpdatePostfix()
+        {
+            bool isInside = Singleton<PlayerController>.Instance.IsInside;
+            if (isInside != hiddenBuildings)
+            {
+                // show these buildings only when outside, they collide with inside the expanded store
+                GameObject.Find("---ENVIRONMENT---/NewYorkCity/Houses/NYC_Building_15_1/SM_NYC_Building_15")?.SetActive(!isInside);
+                GameObject.Find("---ENVIRONMENT---/NewYorkCity/Houses/NYC_Building_8_1 (10)/SM_NYC_Building_8")?.SetActive(!isInside);
+                GameObject.Find("---ENVIRONMENT---/NewYorkCity/Houses/NYC_Building_8_2 (6)/SM_NYC_Building_8")?.SetActive(!isInside);
+                hiddenBuildings = isInside;
+            }
+        }
+
+    }
+
+    [HarmonyPatch]
+    class SectionManagerPatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(SectionManager), "Awake")]
+        public static void AwakePrefix(SectionManager __instance)
+        {
+            // move walls
+            GameObject wallsContainer = GameObject.Find("/---GAME---/Store/Store &&/Walls");
+            foreach (int i in new int[] { 1, 3, 6, 7, 8 })
+            {
+                wallsContainer.transform.GetChild(i).localPosition = wallsContainer.transform.GetChild(i).localPosition.OffsetX(-12);
+            }
+
+            var CloneWall = (int idx, Vector3 offset) =>
+            {
+                GameObject newWall = GameObject.Instantiate(wallsContainer.transform.GetChild(idx).gameObject);
+                newWall.transform.localPosition = newWall.transform.localPosition + offset;
+                newWall.transform.SetParent(wallsContainer.transform);
+            };
+
+            CloneWall(5, new Vector3(-4, 0, 8));
+            CloneWall(5, new Vector3(-8, 0, 8));
+            CloneWall(5, new Vector3(-12, 0, 8));
+
+            CloneWall(9, new Vector3(-4, 0, 8));
+            CloneWall(9, new Vector3(-8, 0, 8));
+            CloneWall(9, new Vector3(-12, 0, 8));
+
+            Plugin.CloneSections();
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(SectionManager), "Start")]
+        public static void StartPrefix()
+        {
+            ListSections();
+        }
+
+        public static void ListSections()
+        {
+            SectionManager sectionManager = Singleton<SectionManager>.Instance;
+            Plugin.Log.LogMessage("Sections: ");
+            foreach (var section in sectionManager.sections)
+            {
+                Plugin.Log.LogMessage($"{section} - ${section.gameObject} - ${section.gameObject.transform.localPosition}");
+                Traverse t = new Traverse(section);
+
+                GameObject[] toBeDisabledObjs = t.Field("m_ToBeDisabled").GetValue<GameObject[]>();
+                Plugin.Log.LogMessage("ToBeDisabled: ");
+                foreach (var obj in toBeDisabledObjs)
+                {
+                    Plugin.Log.LogMessage($"{obj} - ${obj.transform.localPosition}");
+                    Plugin.ListGameObjects(obj);
+                }
+
+                GameObject[] toBeEnabledObjs = t.Field("m_ToBeEnabled").GetValue<GameObject[]>();
+                Plugin.Log.LogMessage("ToBeEnabled: ");
+                foreach (var obj in toBeEnabledObjs)
+                {
+                    Plugin.Log.LogMessage($"{obj} - ${obj.transform.localPosition}");
+                    Plugin.ListGameObjects(obj);
+                }
+            }
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,8 +1,13 @@
-﻿using BepInEx;
+﻿using __Project__.Scripts.FloorPaintSystem;
+using __Project__.Scripts.Managers;
+using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using MyBox;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace StoreExpansion
 {
@@ -18,6 +23,104 @@ namespace StoreExpansion
             Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
             Plugin.Log = Logger;
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            Plugin.Log.LogInfo($"Scene loaded: {scene.name}");
+            if (scene.name == "Main Scene")
+            {
+                List<SectionSO> sections = Singleton<IDManager>.Instance.Sections;
+                SectionSO sectionToClone = sections[sections.Count - 1];
+
+                for (int i = 0; i < 15; i++)
+                {
+                    SectionSO newSection = ScriptableObject.CreateInstance<SectionSO>();
+                    newSection.name = "SectionClone";
+                    newSection.ID = sectionToClone.ID + i + 1;
+                    newSection.Cost = sectionToClone.Cost;
+                    newSection.LocalizedName = sectionToClone.LocalizedName;
+                    newSection.RequiredStoreLevel = sectionToClone.RequiredStoreLevel;
+                    newSection.RequiredSectionID = sectionToClone.ID + i;
+                    newSection.DailyRentAddition = sectionToClone.DailyRentAddition;
+                    sections.Add(newSection);
+                }
+            }
+        }
+
+        public static void CloneSections()
+        {
+            SectionManager sectionManager = Singleton<SectionManager>.Instance;
+            Shader shader = Shader.Find("Universal Render Pipeline/Lit");
+
+            // Clone Section
+            Section[] sections = sectionManager.transform.GetComponentsInChildren<Section>();
+            GameObject sectionToClone = sections[sections.Length - 1].gameObject;
+
+            GameObject floorsContainer = GameObject.Find("/---GAME---/Store/Store &&/Floors");
+            GameObject floorToClone = floorsContainer.transform.GetChild(floorsContainer.transform.childCount - 1).gameObject;
+
+            GameObject ceilingsContainer = GameObject.Find("/---GAME---/Store/Store &&/Ceiling");
+            GameObject ceilingToClone = ceilingsContainer.transform.GetChild(5).gameObject;
+
+            for (int index = 0; index < 15; index++)
+            {
+                GameObject newSection = GameObject.Instantiate(sectionToClone, sectionToClone.transform.parent);
+                newSection.name = $"SectionClone ({index})";
+
+                for (int i = 0; i < newSection.transform.childCount; i++)
+                {
+                    Transform child = newSection.transform.GetChild(i);
+                    for (int j = 0; j < child.childCount; j++)
+                    {
+                        Transform grandChild = child.GetChild(j);
+                        grandChild.transform.localPosition = grandChild.transform.localPosition.OffsetXZ(-4 * ((index/5)+1), (4-index%5) * 4);
+                    }
+                }
+                newSection.transform.SetParent(sectionManager.transform);
+
+                // Clone Floor
+                Plugin.Log.LogInfo($"cloning floor {floorToClone} - {floorToClone.transform.localPosition}");
+                GameObject newFloor = GameObject.Instantiate(floorToClone, floorToClone.transform.parent);
+                newFloor.name = $"FloorClone ({index})";
+                newFloor.transform.localPosition = newFloor.transform.localPosition.OffsetXZ(-4 * ((index / 5) + 1), (4 - index%5) * 4);
+                FloorTextureData defaultFloorTexData = Singleton<PaintManager>.Instance.GetTextureDataFromList(FloorTextureType.TYPE1_DEFAULT);
+                newFloor.GetComponentsInChildren<PaintableFloor>().ForEach(pf => { if (pf.TextureData == null) { pf.Initialize(defaultFloorTexData); } });
+
+
+                // Clone Ceiling
+                Plugin.Log.LogInfo($"cloning ceiling {ceilingToClone} - {ceilingToClone.transform.parent}");
+                GameObject newCeiling = GameObject.CreatePrimitive(PrimitiveType.Plane); //GameObject.Instantiate(ceilingToClone, ceilingToClone.transform.parent);
+                newCeiling.name = $"Fake Ceiling ({index})";
+                newCeiling.tag = ceilingToClone.tag;
+                newCeiling.GetComponent<MeshRenderer>().material = ceilingToClone.GetComponent<MeshRenderer>().materials[1];
+                newCeiling.transform.localRotation = Quaternion.Euler(0, 90, 180);
+                newCeiling.transform.localScale = new Vector3(0.4f, 1, 0.4f);
+                Vector3 offset = new Vector3(-2 + -4 * ((index / 5) + 1), 0,  6 + -4 * (index % 5));
+                newCeiling.transform.localPosition = ceilingToClone.transform.localPosition + offset + new Vector3(0, -0.1f, 0); // ceiling is slightly sunken to architrave level
+                newCeiling.transform.SetParent(ceilingsContainer.transform);
+
+                // pointing up to block shadows
+                GameObject newShadowBlocker = GameObject.CreatePrimitive(PrimitiveType.Plane); //GameObject.Instantiate(ceilingToClone, ceilingToClone.transform.parent);
+                newShadowBlocker.name = $"Fake Ceiling Shadow Blocker ({index})";
+                newShadowBlocker.GetComponent<MeshRenderer>().material = ceilingToClone.GetComponent<MeshRenderer>().materials[0];
+                newShadowBlocker.transform.localRotation = Quaternion.Euler(0, 90, 0);
+                newShadowBlocker.transform.localScale = new Vector3(0.42f, 1, 0.42f); // slightly larger than needed and higher to stop lighting up the architrave
+                newShadowBlocker.transform.localPosition = ceilingToClone.transform.localPosition + offset + new Vector3(0, 0.1f, 0); 
+                newShadowBlocker.transform.SetParent(ceilingsContainer.transform);
+            }
+        }
+
+        public static void ListGameObjects(GameObject obj, string tree = "-")
+        {
+            for (int i = 0; i < obj.transform.childCount; i++)
+            {
+                Transform child = obj.transform.GetChild(i);
+                Plugin.Log.LogMessage($"|{tree} {child.name} - ${child.transform.localPosition}");
+                ListGameObjects(child.gameObject, tree + "-");
+            }
         }
     }
 }
